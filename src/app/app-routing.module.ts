5103c8662ee3e2b1a322dc5458c13ae5d1d0c7d3
shreyashejwalkar <shreyashejwalkar1610@gmail.com>
2020-03-02 15:06:39 +0530

    Open-close animation
    implemented open-close animation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path :"home", component:HomeComponent },
+  {path: "about", component:AboutComponent},
+  {path: "open-close", component:OpenCloseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents= [HomeComponent, AboutComponent, OpenCloseComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OpenCloseComponent } from './open-close/open-close.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    routingComponents,
+    AboutComponent,
+    OpenCloseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
